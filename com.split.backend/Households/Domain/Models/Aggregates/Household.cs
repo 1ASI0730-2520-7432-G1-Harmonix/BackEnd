5374f1c0dce471b6b20c0aef43c0d01125aae4de
hmongus <jose1506.jlmv23@gmail.com>
2025-09-29 20:38:57 -0500

    add command for Household

---
+++ com.split.backend/Households/Domain/Models/Aggregates/Household.cs
@@ -0,0 +1,16 @@
+﻿namespace com.split.backend.Households.Domain.Models.Aggregates;
+
+public partial class Household
+{
+    private long Id { get; }
+    private PersonName Name { get; set; }
+    private string Description { get; set; }
+    
+    private User Representative {get; set;}
+    
+    public Household(){}
+    
+    public Household()
+
+
+}
---
+++ com.split.backend/Households/Domain/Models/Commands/CreateHouseholdCommand.cs
@@ -0,0 +1,45 @@
+﻿using Mysqlx.Crud;
+using System.Collections.Immutable;
+using System.Runtime.CompilerServices;
+
+namespace com.split.backend.Households.Domain.Models.Commands;
+
+public record CreateHouseholdCommand(
+    string Name,
+    string Description,
+    string Currency,
+    long RepresentativeId
+)
+{
+    private static readonly ISet<String>? SUPPORTED_CURRENCIES = new HashSet<String>
+    {
+        "USD",
+        "PEN",
+        "EUR",
+        "GBP",
+    };
+
+    public CreateHouseholdCommand
+    {
+        if (String.IsNullOrEmpty(Name))
+        {
+            throw new ArgumentNullException("Name cannot be null.");
+        }
+
+        if (String.IsNullOrEmpty(Description))
+        {
+            throw new ArgumentException("Description cannot be null.");
+        }
+
+        if (String.IsNullOrEmpty(Currency) || SUPPORTED_CURRENCIES.Contains(Currency))
+        {
+            throw new ArgumentException("Currency cannot be null.");
+        }
+
+        if (RepresentativeId <= 0 || RepresentativeId > Int32.MaxValue || RepresentativeId == null)
+        {
+            throw new ArgumentException("RepresentativeId cannot be null or greater than Int32.MaxValue.");
+        }
+    }
+    
+};
