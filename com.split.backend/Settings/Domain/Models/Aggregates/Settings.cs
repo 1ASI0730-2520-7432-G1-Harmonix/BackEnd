c14fcd4c5acc6ffeb89fa31b187d46fb7f4acac1
hmongus <jose1506.jlmv23@gmail.com>
2025-10-23 19:12:58 -0500

    add Entity settings

---
+++ com.split.backend/Settings/Domain/Models/Aggregates/Settings.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using com.split.backend.Shared.Domain.Models.Aggregates;
+
+namespace com.split.backend.Settings.Domain.Models.Aggregates;
+
+public partial class Settings
+{
+    [Column("Id")]
+    private string Id { get; set; }
+    [Column("UserId")]
+    private long UserId { get; set; }
+    [Column("Language")]
+    private string Language { get; set; }
+    [Column("DarkMode")]
+    private bool DarkMode { get; set; }
+    
+    [Column("NotificationEnabled")]
+    private bool NotificationEnabled { get; set; }
+
+    public Settings(string id, long userId, string language, bool darkMode, bool notificationEnabled)
+    {
+        this.Id = id;
+        this.UserId = userId;
+        this.Language = language;
+        this.DarkMode = darkMode;
+        this.NotificationEnabled = notificationEnabled;
+    }
+}
---
+++ com.split.backend/Settings/Domain/Models/Aggregates/SettingsAudit.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using EntityFrameworkCore.CreatedUpdatedDate.Contracts;
+
+namespace com.split.backend.Settings.Domain.Models.Aggregates;
+
+public partial class Settings : IEntityWithCreatedUpdatedDate
+{
+    [Column("CreatedAt")] public DateTimeOffset? CreatedDate { get; set; }
+    [Column("UpdatedAt")] public DateTimeOffset? UpdatedDate { get; set; }
+}
