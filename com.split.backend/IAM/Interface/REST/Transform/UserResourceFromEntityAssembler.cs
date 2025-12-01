aaf60f1bde4d68c43c98522df193cfcdc7b62d8d
Usuario <jose1506.jlmv23@gmail.com>
2025-11-30 23:55:34 -0500

    chore(): add nullable check for UserResource

--- com.split.backend/IAM/Interface/REST/Transform/UserResourceFromEntityAssembler.cs
+++ com.split.backend/IAM/Interface/REST/Transform/UserResourceFromEntityAssembler.cs
@@ -7,14 +7,30 @@ public static class UserResourceFromEntityAssembler
 {
     public static UserResource ToResourceFromEntity(User entity)
     {
-        return new UserResource(entity.Id,
-            entity.Email.Address,
-            entity.PersonName.FirstName.ToString(),
-            entity.HouseholdId.ToString(),
-                entity.Role.ToString(),
-            entity.Plan.ToString(),
-            entity.Photo.AbsoluteUri,
-            entity.ProfileLockedUntil.ToString(),
-            entity.IsNewUser.Value.ToString());
+        return new UserResource(
+            Id: entity.Id,
+
+            Email: entity.Email?.Address ?? string.Empty,
+
+            PersonName: entity.PersonName?.FirstName 
+                        ?? string.Empty,
+
+            HouseHoldId: entity.HouseholdId ?? string.Empty,
+
+            Role: entity.Role.ToString() 
+                  ?? "Unknown",
+
+            Plan: entity.Plan?.ToString() 
+                  ?? "Unspecified",
+
+            Photo: entity.Photo?.AbsoluteUri 
+                   ?? string.Empty,
+
+            ProfileLockedUntil: entity.ProfileLockedUntil?.ToString("O") 
+                                ?? string.Empty,
+
+            IsNewUser: entity.IsNewUser?.ToString() 
+                       ?? "false"
+        );
     }
 }
